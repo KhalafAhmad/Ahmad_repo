ffc90c1cec87d220e36cd2bcbf2c70990776339f
KhalafAhmad <116549125+KhalafAhmad@users.noreply.github.com>
2023-01-17 11:09:26 +0300

    Create new version of my program ( add while loop )

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include"khalaf_sum.h"
 int main(){
+char check='y' ;	
+while (check=='y'){
 printf("Please Enter first Number: ");
 int Num1 ;
 int Num2 ;
@@ -9,5 +12,10 @@ printf("Please enter second number: ");
 scanf("%d",&Num2);
 int total=KHALAF(Num1,Num2);
 printf("The total resulte for your numbers is= %d\n",total);
+printf ("You need try other? y/n: ");
+scanf("%s",&check);
+if (check=='n')
+exit(0);
+}
 return 0;
 }
